b59ca8d8dde13ab16fa94ee337f24725b0737df3
Víctor Quirino <victorsouzaq@gmail.com>
2020-12-02 21:41:15 -0300

    Rotas, navegação e CRUD parte 3

--- src/pages/User/index.js
+++ src/pages/User/index.js
@@ -1,20 +1,29 @@
 import React,{useState,useEffect} from 'react';
-import api from '../../services/api'
+import api from '../../services/api';
+import {Link} from "react-router-dom";
 import './style.css';
 
 export default function User(){
     const [users,setUsers] = useState([]);
     useEffect(()=>{
-        api.get('users').then(Response =>{
-            setUsers(Response.data);
+        api.get('users').then(response =>{
+            setUsers(response.data);
         })
     },[])
-
+    async function handleDelete(id){
+        try{
+            await api.delete(`/users/${id}`)
+            setUsers(users.filter(user=> user.id !=id))
+        }catch(err){
+            alert('Erro ao deletar!')
+        }
+    }
     return(
         <div id="user-container">
             <h1>Lista de usuários</h1>
+            <Link className="button" id="create-link" to={"/create"}>Criar</Link>
             <ul className="user-list">
-                {users.map(user=>(
+                {users.map(user =>(
                     <li key={user.id}>
                         <strong>Nome</strong>
                         <p>{user.name}</p>
@@ -26,8 +35,8 @@ export default function User(){
                         <p>{user.empresa}</p>
 
                         <div className="actions">
-                            <button className="button" type="button">Deletar</button>
-                            <button className="button" type="button">Acessar</button>
+                            <button className="button" onClick={()=>handleDelete(user.id)} type="button">Deletar</button>
+                            <Link className="button" to={`/update/${user.id}`}>Acessar</Link>
                         </div>
                     </li>
                 ))}
